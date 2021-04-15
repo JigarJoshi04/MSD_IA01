7d5b62b217a35a4f9d3d95bf6dc60fb982832882
JIgar Joshi <jigar.pj@somaiya.edu>
2021-04-15 17:07:09 +0530

    Devops Ready

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3
+ENV PYTHONUNBUFFERED=1
+WORKDIR /code
+COPY requirements.txt /code/
+RUN ls
+RUN pip install -r requirements.txt
+RUN pip install Pillow
+COPY . /code/
---
+++ django_fit/model_final.py
@@ -0,0 +1,17 @@
+import random
+
+
+class ModelPredictor:
+    def ml_search(image_path):
+        model = " I am model"
+        labels = ["bread", "chai", "egg_boiled", "egg_omlet", "rice", "roti", "yellow_dal"]
+
+        return labels[random.randint(0, 7)]
+
+
+# try:
+#     mk =ModelPredictor.ml_search(image_path="/home/vaibhav/Documents/hack_the_mountains/data/test/rice/45.jpg")
+#     print("++", mk, "++")
+
+# except:
+#     print(Exception)
